9da96980a7dc647a25943a4b6cac0a3b68db39ee
pravin1403 <pravinbhapkar01@gmail.com>
2022-08-12 18:51:02 +0530

    SBCWR-5 :create Entity class

--- src/main/java/com/BikkadIT/model/Employee.java
+++ src/main/java/com/BikkadIT/model/Employee.java
@@ -1,5 +1,68 @@
 package com.BikkadIT.model;
 
+import javax.persistence.Entity;
+
+@Entity
 public class Employee {
 
+	private Integer empId;
+	
+	private String empName;
+	
+	private String empAddress;
+	
+	private String empEmail;
+	
+	private Integer empAge;
+
+	public Integer getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(Integer empId) {
+		this.empId = empId;
+	}
+
+	public String getEmpName() {
+		return empName;
+	}
+
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+
+	public String getEmpAddress() {
+		return empAddress;
+	}
+
+	public void setEmpAddress(String empAddress) {
+		this.empAddress = empAddress;
+	}
+
+	public String getEmpEmail() {
+		return empEmail;
+	}
+
+	public void setEmpEmail(String empEmail) {
+		this.empEmail = empEmail;
+	}
+
+	public Integer getEmpAge() {
+		return empAge;
+	}
+
+	public void setEmpAge(Integer empAge) {
+		this.empAge = empAge;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [empId=" + empId + ", empName=" + empName + ", empAddress=" + empAddress + ", empEmail="
+				+ empEmail + ", empAge=" + empAge + "]";
+	}
+	
+	
+	
+	
+	
 }
